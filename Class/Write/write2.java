a6c4c5a7f5e2dd0b0c2a63aed701e2b0ddeb6b61
VS_code For Mr_CHTO <Mr_CHTO@outlook.com>
2020-11-22 21:23:27 +0800

    1122

--- Class/Write/write2.java
+++ Class/Write/write2.java
@@ -1,6 +1,6 @@
 /*
 2、现有一字符串数组如下：
- private String s[] = { " Who "," is ", " the ", " champion "," of ", "the ","Russian ","Cup?", };
+private String s[] = { " Who "," is ", " the ", " champion "," of ", "the ","Russian ","Cup?", };
 要求：
 （1）、定义一个线程类，输出该字符串数组内容；
 （2）、在main函数启动四个线程输出该字符串数组的内容，
@@ -9,6 +9,37 @@
 */
 package Class.Write;
 
-public class write2 {
+import java.nio.channels.spi.SelectorProvider;
 
+//import java.lang.Thread;
+
+class Ot extends Thread {
+    private String[] s = new String[8];
+    private int i = 0;
+
+    public void run() {
+        while (i < 1) {
+            s[0] = " Who ";
+            s[1] = " is ";
+            s[2] = " the ";
+            s[3] = " champion ";
+            s[4] = " of ";
+            s[5] = "the ";
+            s[6] = "Russian ";
+            s[7] = "Cup?";
+            i++;
+            for (String a : s) {
+                System.out.print(a);
+            }
+        }
+    }
+}
+
+public class write2 {
+    public static void main(String[] args) {
+        new Ot().start();
+        new Ot().start();
+        new Ot().start();
+        new Ot().start();
+    }
 }
