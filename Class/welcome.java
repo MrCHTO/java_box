c41eae8195e6223e25bc46fb9673b1a7132218ce
VS_code For Mr_CHTO <Mr_CHTO@outlook.com>
2020-11-22 16:42:05 +0800

    1122

---
+++ Class/welcome.java
@@ -0,0 +1,12 @@
+package Class;
+
+public class welcome {
+    public static void main(String[] args) {
+        String[] greeting = new String[3];
+        greeting[0] = "welocme tocore java";
+        greeting[1] = "by Mr_CHTO";
+        greeting[2] = "and Gary Cornell";
+        for (String g : greeting)
+            System.out.println(g);
+    }
+}
